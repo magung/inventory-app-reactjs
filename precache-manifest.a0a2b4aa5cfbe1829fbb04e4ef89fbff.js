86caa63468f689d6bbe698960e0596116ebee596
magung <agung0401@gmail.com>
2019-10-07 01:52:38 +0700

    Updates

---
+++ precache-manifest.a0a2b4aa5cfbe1829fbb04e4ef89fbff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567248c1db0dbb0389e01c90afcd2e56",
+    "url": "/inventory-app-reactjs/index.html"
+  },
+  {
+    "revision": "262cb3d97c7daa47dca3",
+    "url": "/inventory-app-reactjs/static/css/main.46b63371.chunk.css"
+  },
+  {
+    "revision": "f0a286a70f8ff25e0373",
+    "url": "/inventory-app-reactjs/static/js/2.cb7ce51f.chunk.js"
+  },
+  {
+    "revision": "262cb3d97c7daa47dca3",
+    "url": "/inventory-app-reactjs/static/js/main.8c265c85.chunk.js"
+  },
+  {
+    "revision": "e20c10dc033630070198",
+    "url": "/inventory-app-reactjs/static/js/runtime~main.8419bd76.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/inventory-app-reactjs/static/media/404.6695af99.svg"
+  },
+  {
+    "revision": "1ae00ae4af1d06d9155864837e12bac3",
+    "url": "/inventory-app-reactjs/static/media/Ellipsis-1s-100px.1ae00ae4.gif"
+  },
+  {
+    "revision": "6620b599fd9bd3d3269385432d5b5beb",
+    "url": "/inventory-app-reactjs/static/media/backgroundAddEdit.6620b599.svg"
+  },
+  {
+    "revision": "a7d20743ec1806279de086cd598f137a",
+    "url": "/inventory-app-reactjs/static/media/undraw_deliveries_131a.a7d20743.svg"
+  }
+]);
